43130a0768400dc27f6d5195e5fcf48367e9f6bf
Elis Roberts <elisiforroberts@gmail.com>
2023-08-05 17:58:06 +0100

    feat: :sparkles: Add utility for converting to e3nn irrep notation

--- equiformer/utils.py
+++ equiformer/utils.py
@@ -18,3 +18,14 @@ def generate_rand_features(n_channels: dict[int, int], key: jr.PRNGKey, nodes: i
         A JAX array representing the random feature matrix.
     """
     return {l: jr.uniform(key, (nodes, n, 2 * l + 1)) for l, n in n_channels.items()}
+
+
+def dict_to_e3nn_str(n_channels: dict[int, int]) -> str:
+    """Converts between n_channels dictionary to e3nn irreps notation."""
+    multiplities = [f"{mul}x{irrep}" for irrep, mul in n_channels.items()]
+    return " ".join([multiplities])
+
+
+def e3nn_str_to_dict(e3nn_str: str) -> dict[int, int]:
+    """Converts between e3nn irreps notation and n_channels dictionary."""
+    return {int(irrep): int(mul) for mul, irrep in [item.split('x') for item in e3nn_str.split()]}
